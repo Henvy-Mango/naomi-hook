43faa63496f1ca7e1c868ac1d20903050bf4f7d8
Naomi <mangogo.naomi@qq.com>
2023-02-10 17:19:21 +0800

    chore: upgrade AGP to 7.3.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
+        // maven("https://maven.aliyun.com/repository/google")
+        // maven("https://maven.aliyun.com/repository/public")
+        maven("https://repo.huaweicloud.com/kunpeng/maven")
         google()
         mavenCentral()
         maven("https://api.xposed.info/")
