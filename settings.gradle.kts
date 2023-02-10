1c349d5fec258ad4aaed3bcf2e7b59f1473ee31a
Naomi <mangogo.naomi@qq.com>
2023-02-10 20:46:38 +0800

    chore: upgrade gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // maven("https://maven.aliyun.com/repository/google")
-        // maven("https://maven.aliyun.com/repository/public")
-        maven("https://repo.huaweicloud.com/kunpeng/maven")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+        // maven("https://repo.huaweicloud.com/kunpeng/maven")
         google()
         mavenCentral()
         maven("https://api.xposed.info/")
